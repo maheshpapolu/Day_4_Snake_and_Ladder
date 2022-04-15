428f853b25a877d84db57db280685e9fd35e76fc
maheshpapolu <maheshnaidu9666@gmail.com>
2022-04-15 12:27:40 +0530

    [Mahesh]UC7_Play_the_Game_With_TwoPlayers

---
+++ Snake_and_Ladder_Series/Players.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC7_Play_the_Game_With_TwoPlayers
+{
+     class Player
+    {
+        int[] snake = { 5, 15, 35, 67, 88, 98 };     // Creating array for positions of snakes in board
+        int[] ladder = { 6, 13, 30, 45, 56, 78 };   // Creating array for positions of ladders in board
+        int id;
+        public int currentBoardPosition;
+        public int getDieNumber;
+        public int countDiceRolls;
+        public Player(int id)  // Constructor for class players
+        {
+            this.id = id;
+            currentBoardPosition = 0;
+            countDiceRolls = 0;
+        }
+        public void Dicerolls()     // Object for Random Class
+        {
+            Random random = new Random();   // Creating Object for random Method
+            getDieNumber = random.Next(1, 7);   //Generates random number 1 to 6
+            countDiceRolls++;
+            Console.WriteLine("The number displayed on the die is: " + getDieNumber);
+        }
+        public void Moves()
+        {
+            currentBoardPosition += getDieNumber;
+            Console.WriteLine("\nNow your position is : " + currentBoardPosition);
+            if (Array.Exists(snake, x => x == currentBoardPosition)) //Checking the current position is snake or not
+            {
+                currentBoardPosition -= getDieNumber;   //if it's a snake because of steps behind by dice number
+                Console.WriteLine($"\n YOu reached snake head position, So you moved {getDieNumber} steps behind");
+                Console.WriteLine($"\n Now you are at position : {currentBoardPosition}");
+            }
+            if (Array.Exists(ladder, x => x == currentBoardPosition))    // checking the bottom of the ladder position
+            {
+                Console.WriteLine($"\n You reached at bottom of ladder, So you moved {getDieNumber} steps ahead\n");
+                currentBoardPosition += getDieNumber;   //it,s ladder that's why moves steps a head by dice number
+                Console.WriteLine($"\n Now you are at position : {currentBoardPosition}");
+                Console.WriteLine("\n Again your turn");
+                Dicerolls();
+                Moves();
+            }
+            if (currentBoardPosition > 100)       // Checking that if current position is total board number or above
+            {
+                Console.WriteLine("You get out of range position");
+                currentBoardPosition -= getDieNumber;   //player position remains same if player get out of range position
+                Console.WriteLine($"\n Now you are at position : {currentBoardPosition}");
+            }
+            if (currentBoardPosition < 0) // in case current position is 0 then player restart the game
+            {
+                currentBoardPosition = 0;
+                Console.WriteLine("\nYou are now at positon: " + currentBoardPosition);
+            }
+
+        }
+
+    }
+}
--- Snake_and_Ladder_Series/Program.cs
+++ Snake_and_Ladder_Series/Program.cs
@@ -4,53 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UC6_Report_the_Number_of_Times_the_Dice_was_Played
+namespace UC7_Play_the_Game_With_TwoPlayers
 {
     internal class Program
     {
+        public static int totalBoardNumber = 0;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake & Ladder\n");
             Console.WriteLine("Game Begin\n");
             Console.WriteLine("Currently player at position 0 \n");
-            int totalBoardNumber = 100;
-            int currentBoardPosition = 0;
-            int getDieNumber;
-            int countDiceRolls = 1;
-            int[] snake = { 5, 15, 35, 67, 88, 98 };     // Creating array for positions of snakes in board
-            int[] ladder = { 6, 13, 30, 45, 56, 78 };   // Creating array for positions of ladders in board
-            Random random = new Random();   // Creating Object for random Method
-            while (currentBoardPosition < totalBoardNumber)
+            // Creating objects for 2 players
+            Player player1 = new Player(1);
+            Player player2 = new Player(2);
+            while (true)
             {
-                getDieNumber = random.Next(1, 7);   //Generates random number 1 to 6
-                Console.WriteLine("The number displayed on the die is: " + getDieNumber);
-                currentBoardPosition += getDieNumber;
-                Console.WriteLine("\nNow your position is : " + currentBoardPosition);
-                if (Array.Exists(snake, x => x == currentBoardPosition)) //Checking the current position is snake or not
+                Console.WriteLine("\nPlayer1 turn : ");
+                player1.Dicerolls(); // calling Dicerolls Method for player1 
+                player1.Moves();    //Calling Condition Checking for Player1
+                if (player1.currentBoardPosition == Program.totalBoardNumber)
                 {
-                    currentBoardPosition -= getDieNumber;   //if it's a snake because of steps behind by dice number
-                    Console.WriteLine($"\n YOu reached snake head position, So you moved {getDieNumber} steps behind");
+                    Console.WriteLine("Congratulations...Player1 WON the Game");
+                    break;
+
                 }
-                if (Array.Exists(ladder, x => x == currentBoardPosition))    // checking the bottom of the ladder position
+                // Calling same methods for player2
+                Console.WriteLine("\nPlayer2 turn : ");
+                player2.Dicerolls();
+                player2.Moves();
+                if (player2.currentBoardPosition == Program.totalBoardNumber)
                 {
-                    Console.WriteLine($"\n You reached at bottom of ladder, So you moved {getDieNumber} steps ahead\n");
-                    currentBoardPosition += getDieNumber;   //it,s ladder that's why moves steps a head by dice number
+                    Console.WriteLine("Congratulations...Player2 WON the Game");
+                    break;
                 }
-                if (currentBoardPosition > 100)       // Checking that if current position is total board number or above
-                {
-                    Console.WriteLine("You get out of range position");
-                    currentBoardPosition -= getDieNumber;   //player position remains same if player get out of range position
-                }
-                if (currentBoardPosition < 0) // in case current position is 0 then player restart the game
-                {
-                    currentBoardPosition = 0;
-                }
-                countDiceRolls += 1;    //Count the no of times Dice Rolls the Player to Win The Game
-                Console.WriteLine("No of times Dice Rolls : " + countDiceRolls);
-                
-            }
-                Console.WriteLine("\nYou are now at positon: " + currentBoardPosition);
+                Console.WriteLine("\n Player1 rolls dices : " + player1.countDiceRolls);
+                Console.WriteLine("\n Player2 rolls dices : " + player2.countDiceRolls);
                 Console.ReadLine();
+            }
         }
     }
 }
+            
